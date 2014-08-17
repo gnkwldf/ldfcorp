16a4de7cc700825e0278ba1a7f34e03c8638d962
Gnuk <gnuk@mailoo.org>
2014-08-17 19:35:51 +0200

    Fix timeout problem on poll.js

--- src/GNKWLDF/LdfcorpBundle/Resources/public/js/poll.js
+++ src/GNKWLDF/LdfcorpBundle/Resources/public/js/poll.js
@@ -23,7 +23,7 @@ Vote.prototype.init = function()
                 setTimeout(function() {
                     form.find('input[type="submit"]').prop('disabled', false);
                     form.find('select[name="'+that.option+'"]').prop('disabled', false);
-                }, this.timeout * 1000);
+                }, intVal(this.timeout) * 1000);
             },
             statusCode: {
                 403 : function() {
